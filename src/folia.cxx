6004a256d0f26b8f796e1004aee1addd3decfd65
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2012-01-03 08:48:59 +0000

    added copyright stuff
    git-svn-id: https://ilk.uvt.nl/svn/sources/libfolia/trunk@13851 12f355fe-0486-481a-ad91-c297ab22b4e3

--- src/folia.cxx
+++ src/folia.cxx
@@ -1,3 +1,32 @@
+/*
+  $Id$
+  $URL$
+
+  Copyright (c) 1998 - 2012
+  ILK   - Tilburg University
+  CLiPS - University of Antwerp
+ 
+  This file is part of libfolia
+
+  libfolia is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  libfolia is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+  For questions and suggestions, see:
+      http://ilk.uvt.nl/software.html
+  or send mail to:
+      timbl@uvt.nl
+*/
+
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
