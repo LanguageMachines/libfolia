32a08d8429bff38bf45485493844a0fe23c31984
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2018-11-28 11:12:13 +0100

    argl, never commit without trying 'make' first!

--- src/folia_document.cxx
+++ src/folia_document.cxx
@@ -835,7 +835,7 @@ namespace folia {
 	   << ")\n\t Any possible subsequent failures in parsing or processing may probably be attributed to this." << endl
 	   << "\t Please upgrade libfolia!" << endl;
     }
-    cerr << "reading a " << major_version << "." << minor_version << "." << subversion << " document" << endl;
+    cerr << "reading a " << major_version << "." << minor_version << "." << sub_version << " document" << endl;
     it = kwargs.find( "debug" );
     if ( it != kwargs.end() ){
       debug = TiCC::stringTo<int>( it->second );
