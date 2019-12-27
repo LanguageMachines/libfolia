0092326374ae0deddbc56e89bed151906f1cb022
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2019-12-27 12:28:02 +0100

    added copyright info

--- src/folia_properties.cxx
+++ src/folia_properties.cxx
@@ -1,3 +1,29 @@
+/*
+  Copyright (c) 2006 - 2020
+  CLST  - Radboud University
+  ILK   - Tilburg University
+
+  This file is part of libfolia
+
+  libfolia is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  libfolia is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+  For questions and suggestions, see:
+      https://github.com/LanguageMachines/ticcutils/issues
+  or send mail to:
+      lamasoftware (at ) science.ru.nl
+*/
+
 #include <set>
 #include <string>
 #include <iostream>
