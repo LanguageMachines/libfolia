c0f8381a48defd1fe6fd4a23ae3571562790b34a
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2020-10-16 12:41:17 +0200

    typo

--- src/folia_properties.cxx
+++ src/folia_properties.cxx
@@ -1963,7 +1963,7 @@ namespace folia {
         <strength class="strong"/>
       </sentiment>
       \endverbatim
-      because polarity and strenght ara AttributeFeatures
+      because polarity and strenght are AttributeFeatures
      */
     return AttributeFeatures.find( att ) != AttributeFeatures.end();
   }
