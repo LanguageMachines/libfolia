f207da76e6397df006c61dbd2ab9401404d1f829
Ko van der Sloot <Ko.vanderSloot@ziggo.nl>
2023-09-25 08:26:43 +0200

    signal overriding

--- include/libfolia/folia_subclasses.h
+++ include/libfolia/folia_subclasses.h
@@ -935,7 +935,7 @@ public:								\
     FoliaElement* parseXml( const xmlNode * ) override;
     xmlNode *xml( bool, bool=false ) const override;
     const std::string target() const { return _target; };
-    const std::string content() const { return _content; };
+    const std::string content() const override { return _content; };
   private:
     const UnicodeString private_text( const TextPolicy& ) const override {
       return "";
